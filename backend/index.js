aec186542852d81cf914e8ffc89cd9b83eebc04a
Joshua Okorie <joshuaokorie008@gmail.com>
2023-09-27 11:15:35 +0100

    create signup api route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import moongose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.js";
+import authRouter from "./routes/auth.js";
 
 dotenv.config();
 
@@ -10,9 +11,11 @@ moongose.connect(process.env.MONGO_URI).then(() => {
 }).catch((error)=>console.log(error))
 
 const app = express();
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
 })
 
-app.use("/api/user",userRouter)
+app.use("/api/user", userRouter)
+app.use("/api/auth",authRouter)
