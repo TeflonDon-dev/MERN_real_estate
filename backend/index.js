fa6f723bbd919052bd7b561aacd70e8dda62dbdf
Joshua Okorie <joshuaokorie008@gmail.com>
2023-09-27 09:53:11 +0100

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 import express from "express"
+import moongose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+moongose.connect(process.env.MONGO_URI).then(() => {
+    console.log('connected to the datatbase');
+}).catch((error)=>console.log(error))
 
 const app = express();
 
